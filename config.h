17fabdb5e1c9a7b0b9d9a031966b755a40cee80a
Francesco Minnocci <ad17fmin@uwcad.it>
2020-08-30 18:03:04 +0200

    Fix last commit (colors)

--- config.h
+++ config.h
@@ -138,6 +138,7 @@ static const char *colorname[] = {
   "#add8e6", /* 256 -> cursor */
   "#555555", /* 257 -> rev cursor*/
   "#282828", /* 258 -> bg */
+  "#ebdbb2", /* 259 -> fg */
 };
 
 
@@ -147,8 +148,8 @@ static const char *colorname[] = {
  */
 unsigned int defaultcs = 256;
 unsigned int defaultrcs = 257;
-unsigned int defaultbg = 0;
-unsigned int defaultfg = 15;
+unsigned int defaultbg = 258;
+unsigned int defaultfg = 259;
 
 /*
  * Default shape of cursor
