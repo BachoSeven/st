068749780430f484989b75d3335a0558840b738b
Francesco Minnocci <ad17fmin@uwcad.it>
2020-10-06 11:21:13 +0200

    !!

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static char *font = "scientifica:pixelsize=20:antialias=true:autohint=true";
 static char *font2[] = {
 	"CozetteVector:pixelsize=20:antialias=true:autohint=true",
 	"Mutant Standard emoji:pixelsize=18:antialias=true:autohint=true",
-	"Sarasa Mono K",
+	"Sarasa Mono K:pixelsize=18:antialias=true:autohint=true",
 };
 static int borderperc = 4;
 
